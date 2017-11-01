5737546e7c8284ca665667645d9f50976a0ba1e2
Rohan Mitchell <rohan@rohanmitchell.com>
2017-11-01 15:17:38 +1100

    Graph meditation as minutes per day

--- jobs/meditation.rb
+++ jobs/meditation.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'active_support/time'
 require 'pry'
 
-points = []
+minutes_per_day = {}
 
 CSV.foreach('insight_sessions_export.csv') do |row|
   begin
@@ -11,14 +11,17 @@ CSV.foreach('insight_sessions_export.csv') do |row|
     activity = row[2]
     preset = row[3]
 
-    points << {x: started_at.to_i, y: duration}
+    minutes_per_day[started_at.to_date] ||= 0
+    minutes_per_day[started_at.to_date] += duration.to_i
 
   rescue ArgumentError, TypeError => e
     # Ignore header row where time doesn't parse
   end
 end
 
-points.reverse!
+points = minutes_per_day.keys.sort.map do |day|
+  {x: day.to_time.to_i, y: minutes_per_day[day]}
+end
 
 points_last_month = points.select { |p| p[:x] > 1.month.ago.to_i }
 
