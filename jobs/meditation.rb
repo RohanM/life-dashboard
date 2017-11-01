546d8b668bac8701b15a081ed3ee850d45e1a5cf
Rohan Mitchell <rohan@rohanmitchell.com>
2017-11-01 16:47:44 +1100

    Change all-time graph to last year only, as total hours per month

--- jobs/meditation.rb
+++ jobs/meditation.rb
@@ -3,6 +3,7 @@ require 'active_support/time'
 require 'pry'
 
 minutes_per_day = {}
+minutes_per_month = {}
 
 CSV.foreach('insight_sessions_export.csv') do |row|
   begin
@@ -14,16 +15,26 @@ CSV.foreach('insight_sessions_export.csv') do |row|
     minutes_per_day[started_at.to_date] ||= 0
     minutes_per_day[started_at.to_date] += duration.to_i
 
+    minutes_per_month[started_at.beginning_of_month.to_date] ||= 0
+    minutes_per_month[started_at.beginning_of_month.to_date] += duration.to_i
+
   rescue ArgumentError, TypeError => e
     # Ignore header row where time doesn't parse
   end
 end
 
-points = minutes_per_day.keys.sort.map do |day|
+points_monthly = minutes_per_month.keys.sort.map do |month|
+  {x: month.to_time.to_i, y: minutes_per_month[month] / 60}
+end
+
+points_daily = minutes_per_day.keys.sort.map do |day|
   {x: day.to_time.to_i, y: minutes_per_day[day]}
 end
 
-points_last_month = points.select { |p| p[:x] > 1.month.ago.to_i }
+points_last_year = points_monthly.select { |p| p[:x] > 1.year.ago.to_i }
+points_last_month = points_daily.select { |p| p[:x] > 1.month.ago.to_i }
+points_last_week = points_daily.select { |p| p[:x] > 1.week.ago.to_i }
 
-send_event('meditation-all-time', points: points)
+send_event('meditation-last-year', points: points_last_year)
 send_event('meditation-last-month', points: points_last_month)
+send_event('meditation-last-week', points: points_last_week)
