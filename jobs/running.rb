37d768313553de7951e1c28f90c3211a0c3622db
Rohan Mitchell <rohan@rohanmitchell.com>
2017-11-22 09:01:50 +1100

    Provide more detail, extract interval generation to method

--- jobs/running.rb
+++ jobs/running.rb
@@ -6,18 +6,22 @@ require 'pry'
 
 
 SCHEDULER.every '1m' do
-  workout_dates = []
+  workouts = []
   CSV.foreach('data/map_my_run.csv') do |row|
     next if row[1] == 'Workout Date' # Skip header row
-    workout_dates << Date.parse(row[1])
+    workouts << {date: Date.parse(row[1]), distance: row[4]}
   end
 
-  recent_workouts = workout_dates.sort.last(10)
+  recent_workouts = workouts.sort_by {|w| w[:date]}.last(10)
 
-  intervals = recent_workouts.each_cons(2).map { |a, b| b - a }
-  intervals << Date.today - recent_workouts.last
+  interval_points = intervals(recent_workouts)
 
-  points = intervals.each_with_index.map { |interval, i| {x: i, y: interval.to_i} }
+  send_event('map-my-run-intervals', {points: interval_points, graphtype: 'bar'})
+end
 
-  send_event('map-my-run-intervals', {points: points, graphtype: 'bar'})
+def intervals(workouts)
+  intervals = workouts.each_cons(2).map { |a, b| b[:date] - a[:date] }
+  intervals << Date.today - workouts.last[:date]
+
+  intervals.each_with_index.map { |interval, i| {x: i, y: interval.to_i} }
 end
