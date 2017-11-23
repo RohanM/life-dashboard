a4c0b1d98b6752510850875d8634e6057c1a1387
Rohan Mitchell <rohan@rohanmitchell.com>
2017-11-24 10:26:02 +1100

    Convert running distance graph to pace

--- jobs/running.rb
+++ jobs/running.rb
@@ -8,16 +8,16 @@ SCHEDULER.every '1m' do
   workouts = []
   CSV.foreach('data/map_my_run.csv') do |row|
     next if row[1] == 'Workout Date' # Skip header row
-    workouts << {date: Date.parse(row[1]), distance: row[4].to_f}
+    workouts << {date: Date.parse(row[1]), distance: row[4].to_f, avg_pace: row[6].to_f}
   end
 
   recent_workouts = workouts.sort_by {|w| w[:date]}.last(10)
 
   interval_points = intervals(recent_workouts)
-  distance_points = distances(recent_workouts)
+  pace_points = paces(recent_workouts)
 
   send_event('map-my-run-intervals', {points: interval_points, graphtype: 'bar'})
-  send_event('map-my-run-distances', {points: distance_points, graphtype: 'bar'})
+  send_event('map-my-run-pace', {points: pace_points, graphtype: 'bar'})
 end
 
 def intervals(workouts)
@@ -27,6 +27,6 @@ def intervals(workouts)
   intervals.each_with_index.map { |interval, i| {x: i, y: interval.to_i} }
 end
 
-def distances(workouts)
-  workouts.map { |w| {x: w[:date].to_time.to_i, y: w[:distance]} }
+def paces(workouts)
+  workouts.map { |w| {x: w[:date].to_time.to_i, y: w[:avg_pace]} }
 end
