5aa72b0adb613e1e0912e7f69d36585de05f6d2a
Rohan Mitchell <rohan@rohanmitchell.com>
2017-12-03 20:19:32 +1100

    Convert nomie parser to class

--- jobs/nomie.rb
+++ jobs/nomie.rb
@@ -7,47 +7,54 @@ require 'pry'
 # https://nomie.io/
 
 SCHEDULER.every '1m' do |job|
-  data = JSON.parse(File.read('data/nomie.json'))
-  trackers = load_trackers(data)
-  events = load_events(data)
+  nomie = Nomie.new
 
-  send_intervals_for_tracker(trackers, events, "Feeling connected / supported", 'nomie-social-intervals')
+  nomie.send_intervals_for_tracker("Feeling connected / supported", 'nomie-social-intervals')
 end
 
-def load_trackers(data)
-  Hash[
-    data['trackers'].map { |tracker|
-      [tracker['_id'], tracker]
+
+class Nomie
+  def initialize
+    data = JSON.parse(File.read('data/nomie.json'))
+    @trackers = load_trackers(data)
+    @events = load_events(data)
+  end
+
+  def load_trackers(data)
+    Hash[
+      data['trackers'].map { |tracker|
+        [tracker['_id'], tracker]
+      }
+    ]
+  end
+
+  def load_events(data)
+    data['events'].map { |event|
+      # The tracker id is stored in the event id, deliniated by '|'s
+      tracker_id = event['_id'].split('|')[3]
+      event.merge({
+        'tracker_id' => tracker_id,
+        'time' => Time.at(event['time'] / 1000)
+      })
     }
-  ]
-end
+  end
 
-def load_events(data)
-  data['events'].map { |event|
-    # The tracker id is stored in the event id, deliniated by '|'s
-    tracker_id = event['_id'].split('|')[3]
-    event.merge({
-      'tracker_id' => tracker_id,
-      'time' => Time.at(event['time'] / 1000)
-    })
-  }
-end
+  def events_for_tracker(tracker_label)
+    @events.select { |event|
+      @trackers[event['tracker_id']]['label'] == tracker_label
+    }
+  end
 
-def events_for_tracker(trackers, events, tracker_label)
-  events.select { |event|
-    trackers[event['tracker_id']]['label'] == tracker_label
-  }
-end
+  def event_intervals(events)
+    intervals = events.each_cons(2).map { |a, b| b['time'].to_date - a['time'].to_date }
+    intervals << Date.today - events.last['time'].to_date
 
-def event_intervals(events)
-  intervals = events.each_cons(2).map { |a, b| b['time'].to_date - a['time'].to_date }
-  intervals << Date.today - events.last['time'].to_date
+    intervals.each_with_index.map { |interval, i| {x: i, y: interval.to_i} }
+  end
 
-  intervals.each_with_index.map { |interval, i| {x: i, y: interval.to_i} }
-end
-
-def send_intervals_for_tracker(trackers, events, tracker_label, smashing_event_name)
-  tracker_events = events_for_tracker(trackers, events, tracker_label)
-  interval_points = event_intervals(tracker_events.last(10))
-  send_event(smashing_event_name, {points: interval_points, graphtype: 'bar'})
+  def send_intervals_for_tracker(tracker_label, smashing_event_name)
+    tracker_events = events_for_tracker(tracker_label)
+    interval_points = event_intervals(tracker_events.last(10))
+    send_event(smashing_event_name, {points: interval_points, graphtype: 'bar'})
+  end
 end
