d6f46d4c9bc41d972038f98d6033e6e711d10c40
Rohan Mitchell <rohan@rohanmitchell.com>
2018-12-23 09:41:44 +1100

    Add KM per month running graph

--- jobs/running.rb
+++ jobs/running.rb
@@ -11,13 +11,29 @@ SCHEDULER.every '1m' do
     workouts << {date: Date.parse(row[1]), distance: row[4].to_f, avg_pace: row[6].to_f}
   end
 
+  km_per_month_points = monthly_km_points(workouts)
+
   recent_workouts = workouts.sort_by {|w| w[:date]}.last(10)
 
   interval_points = intervals(recent_workouts)
   pace_points = paces(recent_workouts)
 
+  send_event('map-my-run-monthly', {points: km_per_month_points, graphtype: 'bar'})
   send_event('map-my-run-intervals', {points: interval_points, graphtype: 'bar'})
   send_event('map-my-run-pace', {points: pace_points, y_min: 4.75})
+end
+
+def monthly_km_points(workouts)
+  km_per_month = workouts.inject({}) do |kpm, workout|
+    month = workout[:date].beginning_of_month.to_date
+    kpm[month] ||= 0
+    kpm[month] += workout[:distance]
+    kpm
+  end
+
+  km_per_month.keys.sort.map do |month|
+    {x: month.to_time.to_i, y: km_per_month[month]}
+  end
 end
 
 def intervals(workouts)
