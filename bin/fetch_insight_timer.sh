8e314ac9fee417eefc298e4769b81f6e73a6188e
Rohan Mitchell <rohan@rohanmitchell.com>
2019-11-22 23:41:24 +0000

    Update endpoints for insight timer

--- bin/fetch_insight_timer.sh
+++ bin/fetch_insight_timer.sh
@@ -3,5 +3,5 @@
 COOKIE_FILE=$HOME/tmp/insight_timer.cookie
 mkdir -p $HOME/tmp
 
-curl -s -c $COOKIE_FILE -d "user_session[email]=$1" -d "user_session[password]=$2" https://insighttimer.com/user_session >/dev/null
-curl -b $COOKIE_FILE https://insighttimer.com/sessions/export
+curl -s -c $COOKIE_FILE -d "user_session[email]=$1" -d "user_session[password]=$2" https://profile.insighttimer.com/profile_signin/request >/dev/null
+curl -b $COOKIE_FILE https://profile.insighttimer.com/sessions/export
